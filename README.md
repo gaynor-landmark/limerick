f86709c6ab7c598f5beb2012ae39211cfbb000ed
gaynor-landmark <g.l.landmark@gmail.com>
2016-01-25 22:30:28 +1300

    add limerick creator project

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # limerick
+
+1.  Enter a name, submit and dialay a limerick using that name .
+
+
+
+
+
+2. This will be a random limerick creator.
+The basic structure of the limerick remains the same, but certain phrases will be randomly inserted to create a 'unique' limerick.
+
+A structure like this
+
+There once was a turkey called ............  (randomly insert name/animal) 
+Who...................................(randomly insert what the animal did)
+he........    (something else)
+and ............ (something else)
+and that's why .......
---
+++ jscript.js
@@ -0,0 +1,46 @@
+
+function showLimerick(){
+  //retrieve the name that was entered
+  var name = document.getElementById("name"); 
+   $("#text").empty();      // clear any previous limerick or message ;
+  if (name.value == ""){
+    $("#text").append("Please enter your name so that I  can write you a limerick.");
+  }
+  else {
+    var myLimerick = firstLine(randN()) + name.value + "<br>";    //else, add name to the end of the first line 
+    myLimerick = myLimerick  + limerick.who[randN()] + "<br>";
+    myLimerick = myLimerick  + limerick.andThen[randN()] + "<br>";
+    myLimerick = myLimerick  + limerick.andSo[randN()] + "<br>"; + "!";
+
+    $("#text").append(myLimerick);
+  };
+  // display the limerick text
+  var lim = document.getElementById("limerick");     
+  if (lim.classList.contains("hidden")){
+    lim.classList.remove("hidden");
+  }
+  
+}
+
+function firstLine(n){
+  return "There once was " + limerick.animal[n] + " named ";
+}
+
+function randN(){
+  //return a random number between 0 and 2
+  var x = Math.random();
+  console.log(x);
+  return Math.floor(Math.random() * 3);
+}
+
+var limerick = {
+  animal: ["a horse", "a dog", "a turkey"],
+  who: ["Who went to  vist the zoo,", "Who rode to the beach in a bus,", "Who lived at the top of the hill,"],
+  andThen: ["The sun was too hot and the water too cold,", "They thought he'd escaped, he was caught at the gate", "His house caught alight, on a black stormy night,"],
+  andSo: ["And he never went there again.", "And he nearly passed out from the  pain.", "And he never got home again."]
+
+};
+
+$(document).ready(function(){
+ /* showLimerick();*/
+});
